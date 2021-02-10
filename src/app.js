fb367eb82e06e65e81578917fc11904931aef764
winrid <winrid@gmail.com>
2021-02-10 09:18:32 -0800

    start of building index

--- src/app.js
+++ src/app.js
@@ -4,101 +4,125 @@ const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 const handlebars = require('handlebars');
-const byteSize = require('byte-size');
-const prettyBytes = require('pretty-bytes');
-const readingTime = require('reading-time');
+const {htmlToText} = require('html-to-text');
 
-const YEAR_STR = '2020';
 const CONTENT_DIR = path.join(__dirname, 'content');
+const CATEGORIES_DIR = path.join(__dirname, 'content', 'categories');
+const GUIDES_DIR = path.join(__dirname, 'content', 'guides');
 const TEMPLATE_DIR = path.join(__dirname, 'templates');
 const STATIC_GENERATED_DIR = path.join(__dirname, 'static/generated');
-const STATIC_DIR = path.join(__dirname, 'static');
 
-var posts = [];
-fs.readdirSync(CONTENT_DIR).forEach(function(item) {
-	const title = item.replace('\.md', '');
-	const urlId = title.toLowerCase().replace(/ /g, '-') + '.html';
-	const fullUrl = 'https://blog.winricklabs.com/' + urlId;
+/**
+ * @typedef {Object} IndexEntry
+ * @property {string} url - URL to the index entry.
+ * @property {string} aroundText - Text around the searched word.
+ */
 
-	let fileContent = fs.readFileSync(path.join(CONTENT_DIR, item), 'utf8');
-	fileContent = fileContent.replace('[postlink]', `<a href="${fullUrl}">`);
-	fileContent = fileContent.replace('[/postlink]', `</a>`);	let html = marked(fileContent);
-	const postByteSize = byteSize(html.length).toString();
+/**
+ * @type {Object.<string, Array.<IndexEntry>>}
+ * A map of phrase to relevant results.
+ */
+const index = {};
 
-	const imageSrcs = html.match(/data-src\s*=\s*"?(.+?)["|\s]/g);
-	let mediaSize = 0;
-	if(imageSrcs) {
-		for(const imageSrc of imageSrcs) {
-			mediaSize += fs.statSync(path.join(STATIC_DIR, imageSrc.substring(10, imageSrc.length - 1))).size; // Would be nice to fix regex to not include src=""
-		}
-	}
-	mediaSize = prettyBytes(mediaSize);
-	const fileSizeString = imageSrcs ? `${postByteSize} - ${mediaSize}` : postByteSize;
-	html = html.replace('[filesize]', fileSizeString);
-	const readTime = readingTime(html).text;
-	html = html.replace('[readtime]', readTime);
+/**
+ * @typedef {Object} Content
+ * @property {string} html
+ * @property {string} title
+ * @property {string} urlId
+ * @property {string} fullUrl
+ */
 
-	const dateString = title.substring(1, title.indexOf(')'));
-	const ctime = new Date(dateString);
-	const dateStringReadable = ctime.toDateString();
+/**
+ * @typedef {Object} Meta
+ * @property {string} name
+ * @property {Array.<string>} itemsOrdered
+ */
 
-	html = html.replace('[postdate]', dateStringReadable);
+/**
+ * @param {Content} content
+ */
+function addContentToIndex(content) {
+    const text = htmlToText(content.html, {
+        wordwrap: 9001,
+        tags: {
+            'h1': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'h2': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'h3': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'h4': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'h5': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'h6': {format: 'heading', options: {leadingLineBreaks: 0, trailingLineBreaks: 0, uppercase: false}},
+            'hr': { format: 'horizontalLine', options: { leadingLineBreaks: 0, length: undefined, trailingLineBreaks: 0 } },
+            'p': { format: 'paragraph', options: { leadingLineBreaks: 0, trailingLineBreaks: 0 } },
+        }
+    }).replace(/\n\n/g, '...');
+    const words = text.replace(/\n/g, '').split(' ');
+    for (const word of words) {
+        if (word.length < 4) {
+            continue;
+        }
+        if (word.includes('...')) {
+            continue;
+        }
+        if (index[word] === undefined) {
+            index[word] = [];
+        }
+        const wordPosition = text.indexOf(word);
 
-	let titleNoDate = title.substring(title.indexOf(')') + 1, title.length);
-	if (titleNoDate.startsWith(' - ')) {
-		titleNoDate = titleNoDate.substring(3, titleNoDate.length);
-	}
-
-	posts.push({
-		html: html,
-		title: title,
-		titleNoDate: titleNoDate,
-		urlId: urlId,
-		fullUrl: fullUrl,
-		dateTimeObj: ctime,
-		dateTime: ctime.getTime(),
-		dateStringReadable: dateStringReadable,
-		minSize: postByteSize,
-		mediaSize: mediaSize,
-		fileSizeString: fileSizeString,
-		hasMedia: !!imageSrcs,
-		readTime: readTime
-	});
-});
-
-posts.sort(function(a, b) {
-	if(a.dateTime === b.dateTime) {
-		return 0;
-	}
-	return a.dateTime > b.dateTime ? -1 : 1;
-});
-
-let footerYears = YEAR_STR;
-if(new Date().getFullYear() > YEAR_STR) {
-	footerYears += ' - ' + new Date().getFullYear();
+        index[word].push({
+            url: content.fullUrl,
+            // TODO contain all instances of the word
+            aroundText: text.substring(wordPosition - 100, wordPosition) + '...' + text.substring(wordPosition, wordPosition + 100)
+        });
+    }
 }
 
-fs.readdirSync(TEMPLATE_DIR).forEach(function(item) {
-	if(item === 'index.html') {
-		fs.writeFileSync(path.join(STATIC_GENERATED_DIR, item), getCompiledTemplate(item, {
-			posts: posts,
-				footerYears: footerYears
-		}), 'utf8');
-	}
-	else if (item === 'post.html') {
-		posts.forEach(function(post) {
-			const html = getCompiledTemplate(item, {
-				post: post,
-				posts: posts,
-				footerYears: footerYears
-			});
-			fs.writeFileSync(path.join(STATIC_GENERATED_DIR, post.urlId), html, 'utf8');
-		});
-	}
+const content = {}; // full path like "categories/custom-styles/custom-css.md" to
+fs.readdirSync(CATEGORIES_DIR).forEach(function (category) {
+    /** @type {Meta} **/
+    const meta = JSON.parse(fs.readFileSync(path.join(CATEGORIES_DIR, category, 'meta.json'), 'utf8'));
+    meta.itemsOrdered.forEach((item) => {
+        const title = item.replace('\.md', '');
+        const urlId = title.toLowerCase().replace(/ /g, '-') + '.html';
+        const fullUrl = 'https://docs.fastcomments.com/' + urlId;
+
+        const fileContent = fs.readFileSync(path.join(CATEGORIES_DIR, category, 'items', item), 'utf8');
+        const html = marked(fileContent);
+
+        const entry = {
+            html,
+            title,
+            urlId,
+            fullUrl,
+        };
+
+        addContentToIndex(entry);
+
+        content[`categories/${category}/${item}`] = entry;
+    });
 });
 
-function getCompiledTemplate(templateName, data) {
-	return handlebars.compile(fs.readFileSync(path.join(TEMPLATE_DIR, templateName), 'utf8'))(data);
-}
+fs.writeFileSync(path.join(STATIC_GENERATED_DIR, 'index.json'), JSON.stringify(index), 'utf8');
+
+// fs.readdirSync(TEMPLATE_DIR).forEach(function (item) {
+//     if (item === 'index.html') {
+//         fs.writeFileSync(path.join(STATIC_GENERATED_DIR, item), getCompiledTemplate(item, {
+//             posts: posts,
+//             footerYears: footerYears
+//         }), 'utf8');
+//     } else if (item === 'page.html') {
+//         posts.forEach(function (page) {
+//             const html = getCompiledTemplate(item, {
+//                 page: page,
+//                 posts: posts,
+//                 footerYears: footerYears
+//             });
+//             fs.writeFileSync(path.join(STATIC_GENERATED_DIR, page.urlId), html, 'utf8');
+//         });
+//     }
+// });
+//
+// function getCompiledTemplate(templateName, data) {
+//     return handlebars.compile(fs.readFileSync(path.join(TEMPLATE_DIR, templateName), 'utf8'))(data);
+// }
 
 console.log(`Execution Time: ${Date.now() - startTime}ms`);
---
+++ src/content/categories/custom-styles/items/custom-css.md
@@ -0,0 +1,9 @@
+## A Perfect Fit
+
+When integrating third party components like FastComments with your site or application, care should be taken 
+to ensure your site has a seamless theme and design.
+
+FastComments is designed around the idea that it should be made to fit your site, rather than forcing your
+site to accommodate its design.
+
+...todo...
