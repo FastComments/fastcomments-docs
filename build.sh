c25c778f54c7bdece098fa4c2b8de3ca27ec0426
winrid <winrid@gmail.com>
2021-02-16 07:43:34 -0800

    removing unused code

--- build.sh
+++ build.sh
@@ -7,4 +7,3 @@ cp -rv src/static/css src/static/generated/
 cp -rv src/static/images src/static/generated/
 cp -rv src/static/js src/static/generated/
 cp src/static/*.html src/static/generated/
-#node_modules/uglify-js/bin/uglifyjs node_modules/lozad/dist/lozad.js src/static/js/blog.js -o src/static/generated/js/blog.min.js
