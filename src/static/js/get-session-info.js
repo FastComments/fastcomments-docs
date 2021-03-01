591b318aa40dfe0b9d435467eb2e5101a489bfd0
winrid <winrid@gmail.com>
2021-02-28 17:05:22 -0800

    fixing tenant id check

--- src/static/js/get-session-info.js
+++ src/static/js/get-session-info.js
@@ -1,7 +1,8 @@
 (function () {
     window.getSessionInfo = function getSessionInfo(cb) {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://fastcomments.com/auth/session-info');
+        xhr.withCredentials = true;
+        xhr.open('GET',  location.hostname === 'localhost' ? 'http://localhost:3001/auth/session-info' : 'https://fastcomments.com/auth/session-info');
         xhr.onload = function () {
             if (xhr.status === 200) {
                 try {
