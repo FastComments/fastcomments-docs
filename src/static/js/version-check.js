48b3fba88f94706a26e3c2c78974ebf1131882e2
winrid <winrid@gmail.com>
2021-03-05 08:02:48 -0800

    fixing new build check not always refreshing page and getting latest build id

--- src/static/js/version-check.js
+++ src/static/js/version-check.js
@@ -1,7 +1,4 @@
 (function () {
-    if (!window.buildId) {
-        return console.error('Cannot watch for new version due to missing buildId on page.');
-    }
 
     function getContent(url, cb, cbFailure) {
         const xhr = new XMLHttpRequest();
@@ -21,7 +18,23 @@
             if (responseText && window.buildId !== responseText) {
                 const alertContainer = document.createElement('div');
                 alertContainer.classList.add('new-version-alert');
-                alertContainer.innerHTML = 'A new version of these docs are available! <a href="javascript:window.location.reload(true)">Refresh</a> <a href="javascript:document.querySelector(\'.new-version-alert\').remove()">Ignore This Update</a>';
+                alertContainer.innerHTML = 'A new version of these docs are available! <a href="#" class="refresh">Refresh</a> <a href="#" class="ignore">Ignore This Update</a>';
+                alertContainer.addEventListener('click', function (e) {
+                    if (e.target.classList.contains('refresh')) {
+                        e.preventDefault();
+
+                        const url = new URL(window.location.href);
+                        url.searchParams.set('b', responseText);
+
+                        window.buildId = responseText;
+                        window.location.href = url;
+                    } else if (e.target.classList.contains('ignore')) {
+                        e.preventDefault();
+                        alertContainer.remove();
+                        window.buildId = responseText;
+                        setTimeout(check, 5000);
+                    }
+                });
                 document.body.appendChild(alertContainer);
             } else {
                 setTimeout(check, 5000);
@@ -31,6 +44,8 @@
         });
     }
 
-    // No need to check as soon as they load the page.
-    setTimeout(check, 5000);
+    getContent('/build-id', function success(responseText) {
+        window.buildId = responseText;
+        setTimeout(check, 5000);
+    });
 })();
