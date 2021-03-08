129aaef2e79526b20fa9a2cb07d375a08bea8370
winrid <winrid@gmail.com>
2021-03-08 08:23:49 -0800

    minor refactor

--- src/guides.js
+++ src/guides.js
@@ -22,6 +22,36 @@ const STATIC_GENERATED_DIR = path.join(__dirname, 'static/generated');
  * @property {string} itemsPath
  */
 
+async function buildGuideItem(guide, item, index) {
+    const title = item.name;
+    const id = item.file.replace('.md', '');
+    const urlId = item.file.replace('md', 'html');
+
+    // We add this guide item to the index, but its url is an anchor to the element on the guide page. This way we
+    // can have all the content on one page, but still deep link to it from search nicely.
+    const fullUrl = `/${guide.url}#${id}`;
+
+    const markdown = handlebars.compile(fs.readFileSync(path.join(GUIDES_DIR, guide.id, 'items', item.file), 'utf8'))({
+        ExampleTenantId
+    });
+    let html = marked(await processDynamicContent(markdown, path.join('src', 'content', GUIDES_DIR_NAME, guide.id, 'items', item.file)));
+
+    html += '<style>' + fs.readFileSync(path.join(__dirname, './../node_modules/highlight.js/styles/monokai-sublime.css'), 'utf8') + '</style>';
+
+    addContentToIndex({
+        html,
+        title,
+        urlId,
+        fullUrl
+    }, index);
+
+    return {
+        title,
+        id,
+        content: html
+    };
+}
+
 /**
  *
  * @param {Guide} guide
@@ -33,35 +63,8 @@ async function buildGuide(guide, index) {
     const meta = JSON.parse(fs.readFileSync(path.join(GUIDES_DIR, guide.id, 'meta.json'), 'utf8'));
     const items = [];
     for (const item of meta.itemsOrdered) {
-        const title = item.name;
-        const id = item.file.replace('.md', '');
-        const urlId = item.file.replace('md', 'html');
-
-        // We add this guide item to the index, but its url is an anchor to the element on the guide page. This way we
-        // can have all the content on one page, but still deep link to it from search nicely.
-        const fullUrl = `/${guide.url}#${id}`;
-
-        const markdown = handlebars.compile(fs.readFileSync(path.join(GUIDES_DIR, guide.id, 'items', item.file), 'utf8'))({
-            ExampleTenantId
-        });
-        let html = marked(await processDynamicContent(markdown, path.join('src', 'content', GUIDES_DIR_NAME, guide.id, 'items', item.file)));
-
-        html += '<style>' + fs.readFileSync(path.join(__dirname, './../node_modules/highlight.js/styles/monokai-sublime.css'), 'utf8') + '</style>';
-
-        items.push({
-            title,
-            id,
-            content: html
-        });
-
-        const entry = {
-            html,
-            title,
-            urlId,
-            fullUrl,
-        };
-
-        addContentToIndex(entry, index);
+        // We can't use parallelism here since some of the generators are not "thread safe" - we could fix this by using a pool of browsers in the generators.
+        items.push(await buildGuideItem(guide, item, index));
     }
     const guideIndexPath = path.join(GUIDES_DIR, guide.id, 'index.md.html');
     if (fs.existsSync(guideIndexPath)) {
