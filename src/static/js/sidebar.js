1df7631aacc46fcf860bf80d7a9b61db9ea76cfc
winrid <winrid@gmail.com>
2021-03-16 08:25:02 -0700

    menu on mobile

--- src/static/js/sidebar.js
+++ src/static/js/sidebar.js
@@ -1,4 +1,8 @@
 (function () {
+    const sidebar = document.querySelector('.sidebar');
+    document.querySelector('.sidebar-open-button').addEventListener('click', function() {
+        sidebar.classList.toggle('open');
+    });
     const headings = document.querySelectorAll('.index-guide-item h2');
     let lastVisibleHeadingText = '';
     window.addEventListener('scroll', function () {
@@ -12,16 +16,24 @@
                 if (lastVisibleHeadingText === heading.textContent) {
                     return;
                 }
-                for (const item of document.querySelectorAll('.sidebar-item')) {
+                for (const item of sidebar.querySelectorAll('.sidebar-item')) {
                     if (item.querySelector('a').textContent.trim() === heading.textContent.trim()) {
                         item.classList.add('selected');
-                        document.querySelector('.sidebar').scrollTop = item.offsetTop - 200;
+                        sidebar.scrollTop = item.offsetTop - 200;
                     } else {
                         item.classList.remove('selected');
                     }
                 }
                 lastVisibleHeadingText = heading.textContent;
                 break;
+            }
+        }
+    });
+
+    sidebar.addEventListener('click', function(event) {
+        if (event.target.tagName.toLowerCase() === 'a') {
+            if (sidebar.classList.contains('open')) {
+                sidebar.classList.remove('open');
             }
         }
     });
