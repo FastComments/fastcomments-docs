488aaab7ac4d6fdfff1b8ffa5cd35cb57c730d46
winrid <winrid@gmail.com>
2021-03-22 07:39:43 -0700

    fixing missing script error

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p src/static/images
 mkdir -p src/static/js
 npm install
 rm src/static/generated/*.* # when reusing workspaces on the build server, don't let generated index nodes build up over time
-npm run build
+npm run build-posix
 cp -rv src/static/css src/static/generated/
 cp -rv src/static/images src/static/generated/
 cp -rv src/static/js src/static/generated/
