83726967ff5c0e8e9b13863c06fb8002c807d8af
winrid <winrid@gmail.com>
2021-04-25 18:04:55 -0700

    make missing file

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 mkdir -p src/static/generated
 mkdir -p src/static/css
 mkdir -p src/static/images
+mkdir -p src/static/images/image-cache
 mkdir -p src/static/js
 npm install
 rm -f src/static/generated/*.* # when reusing workspaces on the build server, don't let generated index nodes build up over time. -f flag to ignore errors.
