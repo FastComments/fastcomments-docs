5bee62d5bbb4d258f4195f4dc9fbea140cdf6b9a
winrid <winrid@gmail.com>
2023-01-08 13:29:22 -0800

    can get session info from url if available and 3rd party cookies disabled

--- src/static/js/get-session-info.js
+++ src/static/js/get-session-info.js
@@ -27,13 +27,36 @@
             });
         }
 
-        // check both at the same time for performance
-        const sessionInfo = await Promise.all(sessionEndpoints.map(doGet));
-        for (const info of sessionInfo) {
-            if (info.tenantId) {
-                return cb(info);
+        try {
+            const urlParams = new URLSearchParams(window.location.search);
+            const tenantId = urlParams.get('tId');
+            const region = urlParams.get('region');
+            if (tenantId) {
+                const isProd = location.hostname === 'localhost';
+                const DomainsByRegion = {
+                    'us-west': 'fastcomments.com',
+                    'eu': 'eu.fastcomments.com'
+                };
+                const CDNByRegion = {
+                    'us-west': 'cdn.fastcomments.com',
+                    'eu': 'cdn-eu.fastcomments.com'
+                };
+                return cb({
+                    tenantId,
+                    FC_CDN: region && CDNByRegion[region] ? CDNByRegion[region] : (isProd ? 'https://cdn.fastcomments.com' : 'http://localhost:3001'),
+                    FC_SITE: region && DomainsByRegion[region] ? DomainsByRegion[region] : (isProd ? 'https://fastcomments.com' : 'http://localhost:3001')
+                });
             }
+            // check both at the same time for performance
+            const sessionInfo = await Promise.all(sessionEndpoints.map(doGet));
+            for (const info of sessionInfo) {
+                if (info.tenantId) {
+                    return cb(info);
+                }
+            }
+            return cb(sessionInfo[0]);
+        } catch (e) {
+            console.error(e);
         }
-        cb(sessionInfo[0]);
     }
 })();
