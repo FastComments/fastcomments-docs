a9dbfc8551ac69ae14c1cc42bd7f476cb277964d
winrid <winrid@gmail.com>
2025-09-24 14:19:56 -0700

    load nvm

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 env $(cat /home/winrid/fastcomments-docs/env) node /home/winrid/fastcomments-docs/src/server-search-engine.js >> /tmp/fastcomments-docs-search.log 2>&1;
